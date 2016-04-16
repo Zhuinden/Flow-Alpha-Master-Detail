5d5651f5bac7b7bfebb704130a947e7e97d19191
Zhuinden <zhuinden@gmail.com>
2016-04-16 14:13:21 +0200

    added single pane on portrait

--- app/src/main/java/com/zhuinden/flow_alpha_master_detail/MainActivity.java
+++ app/src/main/java/com/zhuinden/flow_alpha_master_detail/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import com.zhuinden.flow_alpha_master_detail.paths.FirstKey;
@@ -109,10 +110,13 @@ public class MainActivity
 
         View previousView = root.getChildAt(0);
         if(previousView != null) {
-            if(!(previousView instanceof MasterDetailContainer)) {
-                persistViewToState(traversal, previousView);
-            } else {
-                MasterDetailContainer masterDetailContainer = (MasterDetailContainer) root.getChildAt(0);
+            if(previousView instanceof SinglePaneContainer) {
+                SinglePaneContainer singlePaneContainer = (SinglePaneContainer) previousView;
+                if(singlePaneContainer.getChildAt(0) != null) {
+                    persistViewToState(traversal, singlePaneContainer.getChildAt(0));
+                }
+            } else if(previousView instanceof MasterDetailContainer) {
+                MasterDetailContainer masterDetailContainer = (MasterDetailContainer) previousView;
                 RelativeLayout masterContainer = masterDetailContainer.getMasterContainer();
                 RelativeLayout detailContainer = masterDetailContainer.getDetailContainer();
                 if(destKey instanceof IsMaster || destKey instanceof IsFullScreen) {
@@ -121,12 +125,16 @@ public class MainActivity
                 if(destKey instanceof IsMaster || destKey instanceof IsFullScreen || destKey instanceof IsDetail) {
                     persistViewToState(traversal, detailContainer.getChildAt(0));
                 }
+            } else {
+                persistViewToState(traversal, previousView);
             }
         }
 
         Context destContext = traversal.createContext(destKey, this);
         View destView = LayoutInflater.from(destContext).inflate(destKey.getLayout(), root, false);
+
         restoreViewFromState(traversal, destView);
+
         if(destKey instanceof IsFullScreen) {
             if(previousView != null) {
                 root.removeView(previousView);
@@ -135,48 +143,51 @@ public class MainActivity
             callback.onTraversalCompleted();
         } else {
             View currentContainer = root.getChildAt(0);
-            MasterDetailContainer masterDetailContainer = null;
-            if(!(currentContainer instanceof MasterDetailContainer)) {
+            if(!(currentContainer instanceof MasterDetailContainer || currentContainer instanceof SinglePaneContainer)) {
                 if(currentContainer != null) {
                     root.removeView(currentContainer);
                 }
-                masterDetailContainer = (MasterDetailContainer) LayoutInflater.from(this)
-                        .inflate(R.layout.layout_master_detail_container, root, false);
-                root.addView(masterDetailContainer);
-            } else {
-                masterDetailContainer = (MasterDetailContainer) currentContainer;
+                currentContainer = LayoutInflater.from(this).inflate(R.layout.layout_master_detail_container, root, false);
+                root.addView(currentContainer);
             }
-            masterDetailContainer.getDetailContainer().removeAllViews();
-            if(destKey instanceof IsMaster) {
-                masterDetailContainer.getMasterContainer().removeAllViews();
-                masterDetailContainer.getMasterContainer().addView(destView);
 
-            } else if(destKey instanceof IsDetail) {
-                Iterator<IsLayoutKey> destinationHistory = destination.reverseIterator();
-                IsMaster currentMaster = null;
-                while(destinationHistory.hasNext()) {
-                    IsLayoutKey iterHistory = destinationHistory.next();
-                    if(iterHistory instanceof IsMaster) {
-                        currentMaster = (IsMaster) iterHistory;
-                    }
-                }
-                if(currentMaster == null) { //TODO: ensure that adding a detail without previous master will still open master!
-//                    Flow flow = Flow.get(getBaseContext());
-//                    flow.replaceTop(((IsDetail) destKey).getMaster(), Direction.FORWARD);
-//                    flow.set(destKey);
-//                    return;
-                    throw new IllegalStateException("Cannot open directly without adding its master [" + ((IsDetail)destKey).getMaster() + " first!");
-                }
+            if(currentContainer instanceof MasterDetailContainer) {
+                MasterDetailContainer masterDetailContainer = (MasterDetailContainer) currentContainer;
                 masterDetailContainer.getDetailContainer().removeAllViews();
-                masterDetailContainer.getDetailContainer().addView(destView);
-                View currentMasterView = masterDetailContainer.getMasterContainer().getChildAt(0);
-                if(currentMasterView == null) {
-                    Context masterContext = traversal.createContext(currentMaster, this);
-                    View masterView = LayoutInflater.from(masterContext)
-                            .inflate(currentMaster.getLayout(), masterDetailContainer.getMasterContainer(), false);
-                    restoreViewFromState(traversal, masterView);
-                    masterDetailContainer.getMasterContainer().addView(masterView);
+                if(destKey instanceof IsMaster) {
+                    masterDetailContainer.getMasterContainer().removeAllViews();
+                    masterDetailContainer.getMasterContainer().addView(destView);
+                } else if(destKey instanceof IsDetail) {
+                    Iterator<IsLayoutKey> destinationHistory = destination.reverseIterator();
+                    IsMaster currentMaster = null;
+                    while(destinationHistory.hasNext()) {
+                        IsLayoutKey iterHistory = destinationHistory.next();
+                        if(iterHistory instanceof IsMaster) {
+                            currentMaster = (IsMaster) iterHistory;
+                        }
+                    }
+                    if(currentMaster == null) { //TODO: ensure that adding a detail without previous master will still open master!
+//                      Flow flow = Flow.get(getBaseContext());
+//                      flow.replaceTop(((IsDetail) destKey).getMaster(), Direction.FORWARD);
+//                      flow.set(destKey);
+//                      return;
+                        throw new IllegalStateException("Cannot open directly without adding its master [" + ((IsDetail) destKey).getMaster() + " first!");
+                    }
+                    View currentMasterView = masterDetailContainer.getMasterContainer().getChildAt(0);
+                    if(currentMasterView == null) {
+                        Context masterContext = traversal.createContext(currentMaster, this);
+                        View masterView = LayoutInflater.from(masterContext)
+                                .inflate(currentMaster.getLayout(), masterDetailContainer.getMasterContainer(), false);
+                        restoreViewFromState(traversal, masterView);
+                        masterDetailContainer.getMasterContainer().addView(masterView);
+                    }
+                    masterDetailContainer.getDetailContainer().removeAllViews();
+                    masterDetailContainer.getDetailContainer().addView(destView);
                 }
+            } else if(currentContainer instanceof SinglePaneContainer) {
+                SinglePaneContainer singlePaneContainer = (SinglePaneContainer)currentContainer;
+                singlePaneContainer.removeAllViews();
+                singlePaneContainer.addView(destView);
             }
             callback.onTraversalCompleted();
         }
@@ -188,8 +199,11 @@ public class MainActivity
         if(root != null) {
             View currentView = root.getChildAt(0);
             if(currentView instanceof MasterDetailContainer) {
-                MasterDetailContainer masterDetailContainer = (MasterDetailContainer)currentView;
+                MasterDetailContainer masterDetailContainer = (MasterDetailContainer) currentView;
                 ForceBundler.saveToBundle(this, masterDetailContainer.getMasterContainer(), masterDetailContainer.getDetailContainer());
+            } else if(currentView instanceof SinglePaneContainer) {
+                SinglePaneContainer singlePaneContainer = (SinglePaneContainer)currentView;
+                ForceBundler.saveToBundle(this, singlePaneContainer.getChildAt(0));
             } else {
                 ForceBundler.saveToBundle(this, currentView);
             }
